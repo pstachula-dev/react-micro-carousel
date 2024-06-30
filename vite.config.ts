7184aabcce284fe29430136dad710508e628180d
pstachula <pawel.stachula.mail@gmail.com>
2024-06-30 22:14:44 +0200

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,51 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+// import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+// import dts from "vite-plugin-dts";
 
 //dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["src/lib"] })],
+  plugins: [react()],
   build: {
-    sourcemap: true,
-    minify: true,
-    lib: {
-      entry: resolve(__dirname, "src/lib/simple-headless-carousel/index.ts"),
-      formats: ["es"],
-      name: "MyLib",
-      fileName: "simple-headless-carousel",
-    },
-    rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+    // lib: {
+    //   entry: resolve(__dirname, "src/lib/simple-headless-carousel/index.ts"),
+    //   formats: ["es"],
+    //   name: "MyLib",
+    //   fileName: "simple-headless-carousel",
+    // },
+    // rollupOptions: {
+    //   external: ["react", "react/jsx-runtime"],
+    //   output: {
+    //     globals: {
+    //       react: "React",
+    //       "react-dom": "ReactDOM",
+    //     },
+    //   },
+    // },
   },
 });
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), dts({ include: ["src/lib"] })],
+//   build: {
+//     sourcemap: true,
+//     minify: true,
+//     lib: {
+//       entry: resolve(__dirname, "src/lib/simple-headless-carousel/index.ts"),
+//       formats: ["es"],
+//       name: "MyLib",
+//       fileName: "simple-headless-carousel",
+//     },
+//     rollupOptions: {
+//       external: ["react", "react/jsx-runtime"],
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//     },
+//   },
+// });
