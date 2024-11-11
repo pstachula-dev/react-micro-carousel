f9b20487e5be309b007ba04b0326f481186a9f1d
pawel.stachula <pawel.stachula.mail@gmail.com>
2024-11-11 22:18:50 +0100

    fix: lint issues

--- packages/react-simple-headless-carousel/src/lib/simple-headless-carousel/tests/carousel.test.tsx
+++ packages/react-simple-headless-carousel/src/lib/simple-headless-carousel/tests/carousel.test.tsx
@@ -1,13 +1,4 @@
-import {
-  expect,
-  describe,
-  it,
-  vi,
-  beforeEach,
-  afterEach,
-  beforeAll,
-  afterAll,
-} from 'vitest';
+import { expect, describe, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
 import {
